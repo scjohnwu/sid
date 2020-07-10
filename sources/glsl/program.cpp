58857ee8304002ab6f765dbb493a9658745aeaf4
Ivan Sachkov <scvtech@gmail.com>
2020-07-11 02:45:19 +0300

    Add uniform setter for program

--- sources/glsl/program.cpp
+++ sources/glsl/program.cpp
@@ -10,7 +10,7 @@ bool Program::Compile() {
         return true;
     }
 
-    for (auto& shader : m_Shaders) {
+    for (auto &shader : m_Shaders) {
         auto shader_comp = shader->Compile();
         if (!shader_comp) {
             return false;
@@ -26,4 +26,172 @@ bool Program::Compile() {
 }
 
 void Program::Apply() const { glUseProgram(m_ProgramId); }
+
+// Maps to glUniformXX with id storage and caching
+void Program::SetUniform1f(const char *attrib_name, GLfloat v0) {
+    auto location = GetLocation(attrib_name);
+
+    glUniform1f(location, v0);
+}
+
+void Program::SetUniform2f(const char *attrib_name, GLfloat v0, GLfloat v1) {
+    auto location = GetLocation(attrib_name);
+
+    glUniform2f(location, v0, v1);
+}
+
+void Program::SetUniform3f(const char *attrib_name, GLfloat v0, GLfloat v1, GLfloat v2) {
+    auto location = GetLocation(attrib_name);
+
+    glUniform3f(location, v0, v1, v2);
+}
+void Program::SetUniform4f(const char *attrib_name, GLfloat v0, GLfloat v1, GLfloat v2,
+                           GLfloat v3) {
+    auto location = GetLocation(attrib_name);
+
+    glUniform4f(location, v0, v1, v2, v3);
+}
+
+void Program::SetUniform1i(const char *attrib_name, GLint v0) {
+    auto location = GetLocation(attrib_name);
+
+    glUniform1i(location, v0);
+}
+
+void Program::SetlUniform2i(const char *attrib_name, GLint v0, GLint v1) {
+    auto location = GetLocation(attrib_name);
+
+    glUniform2i(location, v0, v1);
+}
+
+void Program::SetUniform3i(const char *attrib_name, GLint v0, GLint v1, GLint v2) {
+    auto location = GetLocation(attrib_name);
+
+    glUniform3i(location, v0, v1, v2);
+}
+
+void Program::SetUniform4i(const char *attrib_name, GLint v0, GLint v1, GLint v2,
+                           GLint v3) {
+    auto location = GetLocation(attrib_name);
+
+    glUniform4i(location, v0, v1, v2, v3);
+}
+
+void Program::SetUniform1ui(const char *attrib_name, GLuint v0) {
+    auto location = GetLocation(attrib_name);
+
+    glUniform1ui(location, v0);
+}
+
+void Program::SetUniform2ui(const char *attrib_name, GLuint v0, GLuint v1) {
+    auto location = GetLocation(attrib_name);
+
+    glUniform2ui(location, v0, v1);
+}
+
+void Program::SetUniform3ui(const char *attrib_name, GLuint v0, GLuint v1, GLuint v2) {
+    auto location = GetLocation(attrib_name);
+
+    glUniform3ui(location, v0, v1, v2);
+}
+
+void Program::SetUniform4ui(const char *attrib_name, GLuint v0, GLuint v1, GLuint v2,
+                            GLuint v3) {
+    auto location = GetLocation(attrib_name);
+
+    glUniform4ui(location, v0, v1, v2, v3);
+}
+
+void Program::SetUniformArray(const char *attrib_name, Array type, GLsizei count,
+                              void_ptr value) {
+    auto location = GetLocation(attrib_name);
+    switch (type) {
+        case Array::fv1:
+            glUniform1fv(location, count, value.c_ptr<GLfloat>());
+            break;
+        case Array::fv2:
+            glUniform2fv(location, count, value.c_ptr<GLfloat>());
+            break;
+        case Array::fv3:
+            glUniform3fv(location, count, value.c_ptr<GLfloat>());
+            break;
+        case Array::fv4:
+            glUniform4fv(location, count, value.c_ptr<GLfloat>());
+            break;
+        case Array::iv1:
+            glUniform1iv(location, count, value.c_ptr<GLint>());
+            break;
+        case Array::iv2:
+            glUniform2iv(location, count, value.c_ptr<GLint>());
+            break;
+        case Array::iv3:
+            glUniform3iv(location, count, value.c_ptr<GLint>());
+            break;
+        case Array::iv4:
+            glUniform4iv(location, count, value.c_ptr<GLint>());
+            break;
+        case Array::uiv1:
+            glUniform1uiv(location, count, value.c_ptr<GLuint>());
+            break;
+        case Array::uiv2:
+            glUniform2uiv(location, count, value.c_ptr<GLuint>());
+            break;
+        case Array::uiv3:
+            glUniform3uiv(location, count, value.c_ptr<GLuint>());
+            break;
+        case Array::uiv4:
+            glUniform4uiv(location, count, value.c_ptr<GLuint>());
+            break;
+        default:
+            break;
+    }
+}
+
+void Program::SetUniformMatrix(const char *attrib_name, Matrix type, GLsizei count,
+                               GLboolean transpose, const GLfloat *value) {
+    auto location = GetLocation(attrib_name);
+
+    switch (type) {
+        case Matrix::fv2:
+            glUniformMatrix2fv(location, count, transpose, value);
+            break;
+        case Matrix::fv3:
+            glUniformMatrix3fv(location, count, transpose, value);
+            break;
+        case Matrix::fv4:
+            glUniformMatrix4fv(location, count, transpose, value);
+            break;
+        case Matrix::fv2x3:
+            glUniformMatrix2x3fv(location, count, transpose, value);
+            break;
+        case Matrix::fv3x2:
+            glUniformMatrix3x2fv(location, count, transpose, value);
+            break;
+        case Matrix::fv2x4:
+            glUniformMatrix2x4fv(location, count, transpose, value);
+            break;
+        case Matrix::fv4x2:
+            glUniformMatrix4x2fv(location, count, transpose, value);
+            break;
+        case Matrix::fv3x4:
+            glUniformMatrix3x4fv(location, count, transpose, value);
+            break;
+        case Matrix::fv4x3:
+            glUniformMatrix4x3fv(location, count, transpose, value);
+            break;
+    }
+}
+
+GLint Program::GetLocation(const char *attrib_name) {
+    auto find_it = m_Locations.find(attrib_name);
+    if (find_it != std::end(m_Locations)) {
+        return find_it->second;
+    }
+
+    auto location = glGetUniformLocation(m_ProgramId, attrib_name);
+    m_Locations[attrib_name] = location;
+
+    return location;
+}
+
 }  // namespace sid
--- sources/glsl/program.h
+++ sources/glsl/program.h
@@ -1,10 +1,38 @@
 #pragma once
 
+#include <unordered_map>
 #include <vector>
 
 #include "shader.h"
 
 namespace sid {
+struct void_ptr {
+    void_ptr(const void *ptr) : m_Ptr{ptr} {};
+
+    void_ptr(const void_ptr &) = delete;
+    void_ptr(void_ptr &&) = delete;
+
+    void_ptr &operator=(const void_ptr &) = delete;
+    void_ptr &operator=(void_ptr &&) = delete;
+
+    const void *m_Ptr{nullptr};
+
+    template <typename T>
+    const T *c_ptr() {
+        return reinterpret_cast<const T *>(m_Ptr);
+    }
+
+    template <typename T>
+    T *ptr() {
+        return reinterpret_cast<T *>(m_Ptr);
+    }
+};
+
+template <typename T>
+void_ptr to_void(const T *ptr) {
+    return void_ptr{reinterpret_cast<const void *>(ptr)};
+}
+
 class Program {
    public:
     Program();
@@ -12,8 +40,38 @@ class Program {
     bool Compile();
     void Apply() const;
 
+    // Maps to glUniformXX with id storage and caching
+    void SetUniform1f(const char *location_name, GLfloat v0);
+    void SetUniform2f(const char *location_name, GLfloat v0, GLfloat v1);
+    void SetUniform3f(const char *location_name, GLfloat v0, GLfloat v1, GLfloat v2);
+    void SetUniform4f(const char *location_name, GLfloat v0, GLfloat v1, GLfloat v2,
+                      GLfloat v3);
+    void SetUniform1i(const char *location_name, GLint v0);
+    void SetlUniform2i(const char *location_name, GLint v0, GLint v1);
+    void SetUniform3i(const char *location_name, GLint v0, GLint v1, GLint v2);
+    void SetUniform4i(const char *location_name, GLint v0, GLint v1, GLint v2, GLint v3);
+    void SetUniform1ui(const char *location_name, GLuint v0);
+    void SetUniform2ui(const char *location_name, GLuint v0, GLuint v1);
+    void SetUniform3ui(const char *location_name, GLuint v0, GLuint v1, GLuint v2);
+    void SetUniform4ui(const char *location_name, GLuint v0, GLuint v1, GLuint v2,
+                       GLuint v3);
+
+    // Maps to glUniformXXfv
+    enum class Array { fv1, fv2, fv3, fv4, iv1, iv2, iv3, iv4, uiv1, uiv2, uiv3, uiv4 };
+    void SetUniformArray(const char *location_name, Array type, GLsizei count,
+                         void_ptr value);
+
+    // Maps to glUniformMatrixXXfv
+    enum class Matrix { fv2, fv3, fv4, fv2x3, fv3x2, fv2x4, fv4x2, fv3x4, fv4x3 };
+    void SetUniformMatrix(const char *location_name, Matrix type, GLsizei count,
+                          GLboolean transpose, const GLfloat *value);
+
    private:
     std::vector<ShaderPtr> m_Shaders;
+    std::unordered_map<const char *, GLint> m_Locations;
+
+    GLint GetLocation(const char *location_name);
+
     GLuint m_ProgramId{0};
     bool m_IsCompiled{false};
 };
